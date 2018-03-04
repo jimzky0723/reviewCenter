a47d0877f336c9388a1d1649c8b4b722ab5e53c1
Jimmy B. Lomocso Jr <jimmy.lomocso@gmail.com>
2018-03-04 21:42:17 +0800

    Minor Fix

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => 'localhost',
             'port' => '3306',
-            'database' => 'jim_center2',
+            'database' => 'jim_center',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
